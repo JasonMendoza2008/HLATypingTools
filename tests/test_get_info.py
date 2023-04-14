13ee1786e6cbd4f026c5eab2da4140916ae703e3
JasonMendoza2008 <lhotteromain@gmail.com>
2023-04-14 18:22:34 +0200

    Better type hinting

--- tests/test_get_info.py
+++ tests/test_get_info.py
@@ -1,21 +1,21 @@
 from hlatypingtools.get_info import get_allele_info, get_locus
 
-def test_get_locus_a():
+def test_get_locus_a() -> None:
     allele = "A*01:01"
     expected_locus = "HLA_A"
     assert get_locus(allele) == expected_locus
 
-def test_get_locus_b():
+def test_get_locus_b() -> None:
     allele = "B*07:02"
     expected_locus = "HLA_B"
     assert get_locus(allele) == expected_locus
 
-def test_get_locus_drb1():
+def test_get_locus_drb1() -> None:
     allele = "DRB1*04:03"
     expected_locus = "HLA_DRB1"
     assert get_locus(allele) == expected_locus
 
-def test_get_locus_c():
+def test_get_locus_c() -> None:
     allele = "C*07:01"
     expected_locus = "HLA_C"
     assert get_locus(allele) == expected_locus
