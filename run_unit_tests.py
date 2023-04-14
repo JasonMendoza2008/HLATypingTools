5f4e124fb04526d58a77aea6ccbb4bac02685a2e
JasonMendoza2008 <lhotteromain@gmail.com>
2023-04-14 16:53:31 +0200

    Removed useless ignores in run_unit_tests.py

--- run_unit_tests.py
+++ run_unit_tests.py
@@ -2,7 +2,7 @@ import pytest
 import sys
 
 if __name__ == "__main__":
-    retcode: int | pytest.ExitCode = pytest.main(["--mypy", "--ignore=Animations", "--ignore=Data Curation"])
+    retcode: int | pytest.ExitCode = pytest.main(["--mypy"])
 
     if retcode == pytest.ExitCode.OK:
         print(f"\n---\nYou are using Python {sys.version} btw\n---\n")
